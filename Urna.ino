37ffa9df311b42fa6c71e4364a749badb0372b10
Antônio Carlos <juniorbecari@gmail.com>
2022-08-29 15:34:28 -0300

    Keypad is working

--- Urna.ino
+++ Urna.ino
@@ -8,19 +8,53 @@
 int pinsLines[] = { 6, 7, 8, 9 };
 int pinsColumns[] = { 10, 11, 12, 13 };
 
-char keys[4][4] = {{'1', '2', '3', 'A'}
-                   {'4', '5', '6', 'B'}
-                   {'7', '8', '9', 'C'}
+char keys[4][4] = {{'1', '2', '3', 'A'},
+                   {'4', '5', '6', 'B'},
+                   {'7', '8', '9', 'C'},
                    {'*', '0', '#', 'D'}};
 
 LiquidCrystal_I2C lcd(address, columns, lines);
 
-void setup() {
+void setup()
+{
     lcd.init();
     lcd.backlight();
     lcd.clear();
+    
+    for (int i = 0; i < 4; i++)
+    {
+        pinMode(pinsLines[i], OUTPUT);
+        digitalWrite(pinsLines[i], HIGH);
+    }
+    
+    for (int i = 0; i < 4; i++)
+    {
+        pinMode(pinsColumns[i], INPUT_PULLUP);
+    }
+    
+    Serial.begin(9600);
+    Serial.println("Waiting...");
 }
 
-void loop() {
+void loop()
+{
+    for (int l = 0; l < 4; l++)
+    {
+        digitalWrite(pinsLines[l], LOW);
+        
+        for (int c = 0; c < 4; c++)
+        {
+            if (digitalRead(pinsColumns[c]) == LOW)
+            {
+                Serial.print("Tecla: ");
+                Serial.println(keys[l][c]);
+                
+                while (digitalRead(pinsColumns[c]) == LOW) {}
+            }
+        }
+        
+        digitalWrite(pinsLines[l], HIGH);
+    }
     
+    delay(10);
 }

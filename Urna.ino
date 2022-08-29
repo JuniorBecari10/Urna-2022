274b1b12ac89df14463747a817971380b79d9708
Antônio Carlos <juniorbecari@gmail.com>
2022-08-29 17:50:49 -0300

    Added library

--- Urna.ino
+++ Urna.ino
@@ -1,3 +1,4 @@
+#include <Keypad.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
@@ -5,27 +6,36 @@
 #define columns  16
 #define lines    2
 
-int pinsLines[] = { 6, 7, 8, 9 };
-int pinsColumns[] = { 10, 11, 12, 13 };
+#define MAX_NAME 17
+
+unsigned char pinsLines[] = { 6, 7, 8, 9 };
+unsigned char pinsColumns[] = { 10, 11, 12, 13 };
 
 char keys[4][4] = {{'1', '2', '3', 'A'},
                    {'4', '5', '6', 'B'},
                    {'7', '8', '9', 'C'},
                    {'*', '0', '#', 'D'}};
 
+Keypad keypad = Keypad(makeKeymap(keys), pinsLines, pinsColumns, 4, 4);
+
 // -----------
 
 struct Candidate
 {
-    String name;
+    char name[MAX_NAME]; // string
     unsigned char number; // byte
     int votes;
 };
-
+/*
+string candPres[] = { "Ciro Gomes", "Eymael", "Felipe D'Ávila",
+                      "Jair Bolsonaro", "Lula", "Léo Péricles",
+                      "Pablo Marçal", "Roberto Jeff.", "Simone Tebet",
+                      "Sofia Manzano", "Soraya Thronicke", "Vera" };
+*/
 Candidate pres[] = { {"Ciro Gomes", 12, 0}, {"Eymael", 27, 0}, {"Felipe D'Ávila", 30, 0},
                      {"Jair Bolsonaro", 22, 0}, {"Lula", 13, 0}, {"Léo Péricles", 80, 0},
                      {"Pablo Marçal", 90, 0}, {"Roberto Jeff.", 14, 0}, {"Simone Tebet", 15, 0},
-                     {"Sofia Manzano", 21, 0}, {"Soraya Thron.", 44, 0}, {"Vera", 16, 0} };
+                     {"Sofia Manzano", 21, 0}, {"Soraya Thronicke", 44, 0}, {"Vera", 16, 0} };
 
 LiquidCrystal_I2C lcd(address, columns, lines);
 
@@ -35,40 +45,16 @@ void setup()
     lcd.backlight();
     lcd.clear();
     
-    for (int i = 0; i < 4; i++)
-    {
-        pinMode(pinsLines[i], OUTPUT);
-        digitalWrite(pinsLines[i], HIGH);
-    }
-    
-    for (int i = 0; i < 4; i++)
-    {
-        pinMode(pinsColumns[i], INPUT_PULLUP);
-    }
-    
-    //Serial.begin(9600);
-    //Serial.println("Waiting...");
+    Serial.begin(9600);
+    Serial.println("Waiting...");
 }
 
 void loop()
 {
-    for (int l = 0; l < 4; l++)
-    {
-        digitalWrite(pinsLines[l], LOW);
-        
-        for (int c = 0; c < 4; c++)
-        {
-            if (digitalRead(pinsColumns[c]) == LOW)
-            {
-                //Serial.print("Tecla: ");
-                //Serial.println(keys[l][c]);
-                
-                while (digitalRead(pinsColumns[c]) == LOW) {}
-            }
-        }
-        
-        digitalWrite(pinsLines[l], HIGH);
-    }
+    char key = keypad.getKey();
     
-    delay(10);
+    if (key)
+    {
+        Serial.println(key);
+    }
 }

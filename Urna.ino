f62d009da52a6687aa6b3349e8a9efbad8fac8a0
Antônio Carlos <juniorbecari@gmail.com>
2022-08-29 17:52:51 -0300

    Adding more candidates

--- Urna.ino
+++ Urna.ino
@@ -6,8 +6,6 @@
 #define columns  16
 #define lines    2
 
-#define MAX_NAME 17
-
 unsigned char pinsLines[] = { 6, 7, 8, 9 };
 unsigned char pinsColumns[] = { 10, 11, 12, 13 };
 
@@ -22,20 +20,17 @@ Keypad keypad = Keypad(makeKeymap(keys), pinsLines, pinsColumns, 4, 4);
 
 struct Candidate
 {
-    char name[MAX_NAME]; // string
+    String name;
     unsigned char number; // byte
     int votes;
 };
-/*
-string candPres[] = { "Ciro Gomes", "Eymael", "Felipe D'Ávila",
-                      "Jair Bolsonaro", "Lula", "Léo Péricles",
-                      "Pablo Marçal", "Roberto Jeff.", "Simone Tebet",
-                      "Sofia Manzano", "Soraya Thronicke", "Vera" };
-*/
+
 Candidate pres[] = { {"Ciro Gomes", 12, 0}, {"Eymael", 27, 0}, {"Felipe D'Ávila", 30, 0},
                      {"Jair Bolsonaro", 22, 0}, {"Lula", 13, 0}, {"Léo Péricles", 80, 0},
                      {"Pablo Marçal", 90, 0}, {"Roberto Jeff.", 14, 0}, {"Simone Tebet", 15, 0},
                      {"Sofia Manzano", 21, 0}, {"Soraya Thronicke", 44, 0}, {"Vera", 16, 0} };
+
+Candidate gov[] = { {"Altino", 16, 0} };
 
 LiquidCrystal_I2C lcd(address, columns, lines);
 

2c3b9c2b59174492bfed7d217fc24fed3fb15579
Antônio Carlos <juniorbecari@gmail.com>
2022-08-29 17:42:40 -0300

    Added votes

--- Urna.ino
+++ Urna.ino
@@ -5,8 +5,6 @@
 #define columns  16
 #define lines    2
 
-#define MAX_NAME 17
-
 int pinsLines[] = { 6, 7, 8, 9 };
 int pinsColumns[] = { 10, 11, 12, 13 };
 
@@ -19,19 +17,15 @@ char keys[4][4] = {{'1', '2', '3', 'A'},
 
 struct Candidate
 {
-    char name[MAX_NAME]; // string
+    String name;
     unsigned char number; // byte
+    int votes;
 };
-/*
-string candPres[] = { "Ciro Gomes", "Eymael", "Felipe D'Ávila",
-                      "Jair Bolsonaro", "Lula", "Léo Péricles",
-                      "Pablo Marçal", "Roberto Jeff.", "Simone Tebet",
-                      "Sofia Manzano", "Soraya Thronicke", "Vera" };
-*/
-Candidate pres[] = { {"Ciro Gomes", 12}, {"Eymael", 27}, {"Felipe D'Ávila", 30},
-                     {"Jair Bolsonaro", 22}, {"Lula", 13}, {"Léo Péricles", 80},
-                     {"Pablo Marçal", 90}, {"Roberto Jeff.", 14}, {"Simone Tebet", 15},
-                     {"Sofia Manzano", 21}, {"Soraya Thronicke", 44}, {"Vera", 16} };
+
+Candidate pres[] = { {"Ciro Gomes", 12, 0}, {"Eymael", 27, 0}, {"Felipe D'Ávila", 30, 0},
+                     {"Jair Bolsonaro", 22, 0}, {"Lula", 13, 0}, {"Léo Péricles", 80, 0},
+                     {"Pablo Marçal", 90, 0}, {"Roberto Jeff.", 14, 0}, {"Simone Tebet", 15, 0},
+                     {"Sofia Manzano", 21, 0}, {"Soraya Thron.", 44, 0}, {"Vera", 16, 0} };
 
 LiquidCrystal_I2C lcd(address, columns, lines);
 
